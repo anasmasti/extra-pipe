02966332d4f7192d5ebfad9489dbb16594c5cb10
anasmasti <anasmasti@hotmail.com>
2023-01-12 16:40:38 +0100

    Update: Refactoring files

--- projects/extra-pipe/src/lib/extra-pipe.module.ts
+++ projects/extra-pipe/src/lib/extra-pipe.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ExtraPipeComponent } from './extra-pipe.component';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
-import { UpperOfPipe } from './pipes/upperOf.pipe';
+import { UpperCaseFromPipe } from './pipes/uppercase-from.pipe';
 
 @NgModule({
-  declarations: [ExtraPipeComponent, CapitalizePipe, UpperOfPipe],
+  declarations: [ExtraPipeComponent, CapitalizePipe, UpperCaseFromPipe],
   imports: [],
-  exports: [ExtraPipeComponent, CapitalizePipe, UpperOfPipe],
+  exports: [ExtraPipeComponent, CapitalizePipe, UpperCaseFromPipe],
 })
 export class ExtraPipeModule {}
