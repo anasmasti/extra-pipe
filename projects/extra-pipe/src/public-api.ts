dfcce3289c0cf5f31928bd2711bc0d4c76506753
anasmasti <anasmasti@hotmail.com>
2023-08-28 16:02:02 +0100

    Update package info

--- projects/extra-pipe/src/public-api.ts
+++ projects/extra-pipe/src/public-api.ts
@@ -2,6 +2,11 @@
  * Public API Surface of extra-pipe
  */
 
-export * from './lib/extra-pipe.service';
-export * from './lib/extra-pipe.component';
-export * from './lib/pipes/capitalize.pipe';
+export * from './lib/shared/pipes/capitalize.pipe';
+export * from './lib/shared/pipes/filesize.pipe';
+export * from './lib/shared/pipes/hide.pipe';
+export * from './lib/shared/pipes/img-url-base64.pipe';
+export * from './lib/shared/pipes/localized-date.pipe';
+export * from './lib/shared/pipes/replace-comma.pipe';
+export * from './lib/shared/pipes/roud-half.pipe';
+export * from './lib/shared/pipes/uppercase-from.pipe';
