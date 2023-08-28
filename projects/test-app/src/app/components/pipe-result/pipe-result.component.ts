7233240f06661d0292a2bbb018e23de1067d4cdd
anasmasti <anasmasti@hotmail.com>
2023-08-28 14:47:36 +0100

    Feat: add replace comma pipe

--- projects/test-app/src/app/components/pipe-result/pipe-result.component.ts
+++ projects/test-app/src/app/components/pipe-result/pipe-result.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RoundHalfUpPipe } from 'projects/extra-pipe/src/lib/shared/pipes/roud-half-up.pipe';
+import { ReplaceCommaPipe } from 'projects/extra-pipe/src/lib/shared/pipes/replace-comma.pipe';
 
 @Component({
   standalone: true,
   selector: 'app-pipe-result',
   templateUrl: './pipe-result.component.html',
-  imports: [RoundHalfUpPipe],
+  imports: [ReplaceCommaPipe],
 })
 export class PipeResultComponent {
   title = 'test-app';
